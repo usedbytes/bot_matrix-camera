7dd1733612b583343fc746a5e8e0776e3d37ec7d
Brian Starkey <stark3y@gmail.com>
2018-04-19 23:07:56 +0100

    main: Add alpha to FBOs

--- main.c
+++ main.c
@@ -134,7 +134,7 @@ static struct fbo *create_fbo(uint32_t width, uint32_t height, GLuint texture)
 
 	fbo->width = width;
 	fbo->height = height;
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, fbo->width, fbo->height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, fbo->width, fbo->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo->texture, 0);
